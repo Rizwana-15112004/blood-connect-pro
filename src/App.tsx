d92b7a5211e05e44bc6202b33803bea73dc8e683
Rizwana Naznin C A <rizwananazninca@gmail.com>
2026-02-01 00:51:46 +0530

    chore: setup github pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -92,7 +92,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <AuthProvider>
             <AuthenticatedApp />
           </AuthProvider>
